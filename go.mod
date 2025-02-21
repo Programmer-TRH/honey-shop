df30bf99d6ca8d508b2c8f8b43448b7f1abb9671
Programmer-TRH <tokdirrahoman4@gmail.com>
2025-02-21 10:09:15 +0600

    v-0.1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Programmer-TRH/hoeny-shop
+
+go 1.23.6
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := strings.TrimPrefix(r.URL.Path, "/")
+		if path == "" || path == "/" {
+			http.ServeFile(w, r, "static/index.html")
+			return
+		}
+		filepath := "static/html" + path + ".html"
+		http.ServeFile(w, r, filepath)
+	})
+
+	log.Println("Server is running on http://localhost:8080")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
