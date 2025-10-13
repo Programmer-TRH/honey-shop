f1bcefd1064bfe9690c5c26946e4b40155609e4b
Programmer-TRH <tokdirrahoman4@gmail.com>
2025-10-13 18:21:58 +0600

    v-0.2.4.4

--- components/admin/product/admin-products.tsx
+++ components/admin/product/admin-products.tsx
@@ -8,7 +8,7 @@ export const revalidate = 3600;
 
 export default async function AdminProducts() {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-  const res = await fetch(`${baseUrl}/api/custom/products?page=1&limit=15`, {
+  const res = await fetch(`${baseUrl}/api/custom/products?`, {
     next: {
       tags: ["products"],
       revalidate: 3600,
